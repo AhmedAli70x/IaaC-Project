02af68ace7108e6c518e73b79ce1b503c2fa031a
Ahmed Ali <Ahmedali70x@gmail.com>
2022-08-05 09:13:29 +0100

    IaaC  for high availability application

---
+++ create_stack.sh
@@ -0,0 +1,5 @@
+
+aws cloudformation package --template-file main.yml --s3-bucket s33final > out.yml
+aws cloudformation create-stack --stack-name app-final-stack  --template-body  file://out.yml   --parameters file://myinfra.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
+
+
