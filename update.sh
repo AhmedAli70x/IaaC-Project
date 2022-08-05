dcf770a16bc9fb1e4a458eefc9b514cdcd591d66
Ahmed Ali <Ahmedali70x@gmail.com>
2022-08-05 20:25:59 +0100

    xs

--- update.sh
+++ update.sh
@@ -1,5 +1,1 @@
-
-aws cloudformation package --template-file main.yml --s3-bucket s33final > out.yml
-aws cloudformation update-stack --stack-name app-final-stack  --template-body  file://out.yml   --parameters file://myinfra.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
-
-
+aws cloudformation update-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
